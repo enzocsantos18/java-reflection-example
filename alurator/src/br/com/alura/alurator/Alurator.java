5737a480ca67d2d6d44642037e5d0cd947258682
Enzo Carvalho <enzocsantos18@gmail.com>
2021-07-13 14:23:16 -0300

    feat: Tratamento url Classe Request

--- alurator/src/br/com/alura/alurator/Alurator.java
+++ alurator/src/br/com/alura/alurator/Alurator.java
@@ -1,11 +1,23 @@
 package br.com.alura.alurator;
 
+import br.com.alura.alurator.protocolo.Request;
+
+import java.util.Map;
+
 public class Alurator {
-	
+
+
+
 	public Object executa(String url) {
 		// TODO - processa a requisicao executando o metodo
 		// da classe em questao
-		
+
+		Request request = new Request(url);
+		String nomeController = request.getNomeController();
+		String nomeMetodo = request.getNomeMetodo();
+		Map<String, Object> params = request.getQueryParams();
+
+
 		return null;
 	}
 }
--- alurator/src/br/com/alura/alurator/protocolo/Request.java
+++ alurator/src/br/com/alura/alurator/protocolo/Request.java
@@ -1,7 +1,39 @@
 package br.com.alura.alurator.protocolo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Request {
-	
-	public Request(String url) {
-	}
+
+    private String nomeController;
+    private String nomeMetodo;
+    private Map<String, Object> queryParams;
+
+
+    public Request(String url) {
+        String[] partesUrl = url.replaceFirst("/", "").split("[?]");
+
+        String[] controleEMetodo = partesUrl[0].split("/");
+
+        this.nomeController = Character.toUpperCase(controleEMetodo[0].charAt(0))
+				+ controleEMetodo[0].substring(1) + "Controller";
+
+        this.nomeController = controleEMetodo[1];
+
+        this.queryParams = partesUrl.length > 1
+				? new QueryParamsBuilder().withParams(partesUrl[1]).build()
+				: new HashMap<String, Object>();
+    }
+
+    public String getNomeController() {
+        return nomeController;
+    }
+
+    public String getNomeMetodo() {
+        return nomeMetodo;
+    }
+
+    public Map<String, Object> getQueryParams() {
+        return queryParams;
+    }
 }
